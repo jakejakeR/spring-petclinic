7e3f1eb65f4cffb755d4c15ef247e556d6260f8e
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-24 19:51:55 +0200

    Lecture 83 - Added Vet List and updated Vet Controller. Closes #15

--- spring-petclinic/spring-petclinic-web/src/main/java/com/petclinic/springpetclinic/controllers/VetController.java
+++ spring-petclinic/spring-petclinic-web/src/main/java/com/petclinic/springpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.petclinic.springpetclinic.controllers;
 
+import com.petclinic.springpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model vetModel) {
+        vetModel.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
