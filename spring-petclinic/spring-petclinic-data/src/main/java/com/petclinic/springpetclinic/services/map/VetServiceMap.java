f814f34e8d96995c3403feb82ccbfeceb7c627cd
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-24 13:19:19 +0200

    Lecture 80 - Load Data on Startup with Java

--- spring-petclinic/spring-petclinic-data/src/main/java/com/petclinic/springpetclinic/services/map/VetServiceMap.java
+++ spring-petclinic/spring-petclinic-data/src/main/java/com/petclinic/springpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.petclinic.springpetclinic.services.map;
 
 import com.petclinic.springpetclinic.model.Vet;
-import com.petclinic.springpetclinic.services.CrudService;
+import com.petclinic.springpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
